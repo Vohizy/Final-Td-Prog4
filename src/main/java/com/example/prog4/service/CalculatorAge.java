2a7d491dc99f10c5e6c8dbdbfa222a281ef0d986
vohizy <hei.vohizy@gmail.com>
2023-08-27 20:50:00 +0300

    feat: class for calculate the age for the employee

---
+++ src/main/java/com/example/prog4/service/CalculatorAge.java
@@ -0,0 +1,19 @@
+package com.example.prog4.service;
+
+import java.time.LocalDate;
+
+public class CalculatorAge {
+    public static int fromBirth(LocalDate birthDate) {
+        if (birthDate == null){
+            return 0;
+        }
+        LocalDate currentDate = LocalDate.now();
+        LocalDate reference = LocalDate.of(currentDate.getYear(), birthDate.getMonth(), birthDate.getDayOfMonth());
+        int birthYear = birthDate.getYear();
+        int age = currentDate.getYear() - birthYear;
+        if(reference.isAfter(birthDate)){
+            age-= 1;
+        }
+        return age;
+    }
+}
