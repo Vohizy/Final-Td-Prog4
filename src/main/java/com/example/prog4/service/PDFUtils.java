9f2cdc0876c0ad49f31a6d7c90c89a2320eeb0ef
vohizy <hei.vohizy@gmail.com>
2023-08-27 20:50:48 +0300

    feat: class to converitire file html to a Pdf

---
+++ src/main/java/com/example/prog4/service/PDFUtils.java
@@ -0,0 +1,46 @@
+package com.example.prog4.service;
+
+import com.example.prog4.config.CompanyConf;
+import com.example.prog4.model.Employee;
+import com.example.prog4.model.exception.InternalServerErrorException;
+import com.lowagie.text.DocumentException;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.context.Context;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.ByteArrayOutputStream;
+
+public class PDFUtils {
+    private static String parseThymeleafTemplate(Employee employee) {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/pdf/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        Context context = new Context();
+        context.setVariable("employee", employee);
+        context.setVariable("companyConf", new CompanyConf());
+
+        return templateEngine.process("employee_pdf", context);
+    }
+
+    public static byte[] pdfFromHtml(Employee employee) {
+        String html = parseThymeleafTemplate(employee);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ITextRenderer render = new ITextRenderer();
+        render.setDocumentFromString(html);
+        render.layout();
+
+        try {
+            render.createPDF(outputStream);
+        } catch (DocumentException e) {
+            throw new InternalServerErrorException(e.getMessage());
+        }
+        return outputStream.toByteArray();
+    }
+}
